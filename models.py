f23be14da09bc381b976e7d5bde7b273671da42a
blaz <blazyy@gmail.com>
2021-05-07 14:18:13 +0200

    no message

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ import os
 from sqla_wrapper import SQLAlchemy
 
 db = SQLAlchemy(os.getenv("DATABASE_URL", "sqlite:///localhost.sqlite"))
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
